a3521257727ed805b80b9ca850cdf7e7d259f928
Keith Powell <powellke@saic.com>
2018-04-11 13:35:30 -0500

    Don't throw an exception when headers have already been sent.

--- web/Response.php
+++ web/Response.php
@@ -363,7 +363,8 @@ class Response extends \yii\base\Response
     protected function sendHeaders()
     {
         if (headers_sent($file, $line)) {
-            throw new HeadersAlreadySentException($file, $line);
+			return;
+            //throw new HeadersAlreadySentException($file, $line);
         }
         if ($this->_headers) {
             foreach ($this->getHeaders() as $name => $values) {
